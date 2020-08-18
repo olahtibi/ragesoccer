fd3149a742de3fd92f1249846c729f3ea59be62f
Tibor Olah <42848540+olahtibi@users.noreply.github.com>
2020-08-18 16:05:19 -0400

    Implement 'pause'

--- io/io.js
+++ io/io.js
@@ -15,37 +15,42 @@ function touchHandler(e) {
 
 function checkInput(e) {
     window.keyMap[e.keyCode] = e.type == 'keydown';
-    // Pixels per second
-    var velocity = window.game.config.playerVelocity;
-    window.game.stadium.playerHome.velocity.x = 0;
-    window.game.stadium.playerHome.velocity.y = 0;
-    // player - home
-    if(window.keyMap[38]) {
-        // console.log('UP');
-        window.game.stadium.playerHome.velocity.y = 0 - velocity;
-        startGame();
-    }
-    if(window.keyMap[40]) {
-        // console.log('DOWN');
-        window.game.stadium.playerHome.velocity.y = velocity;
-        startGame();
-    }
-    if(window.keyMap[37]) {
-        // console.log('LEFT');
-        window.game.stadium.playerHome.velocity.x = 0 - velocity;
-        startGame();
-    }
-    if(window.keyMap[39]) {
-        // console.log('LEFT');
-        window.game.stadium.playerHome.velocity.x = velocity;
-        startGame();
-    }
-    if(window.game.stadium.playerHome.velocity.x != 0 && window.game.stadium.playerHome.velocity.x != 0) {
-        window.game.stadium.playerHome.velocity.x /= Math.sqrt(2);
-        window.game.stadium.playerHome.velocity.y /= Math.sqrt(2);
+    if(!window.game.isPaused()) {
+        // Pixels per second
+        var velocity = window.game.config.playerVelocity;
+        window.game.stadium.playerHome.velocity.x = 0;
+        window.game.stadium.playerHome.velocity.y = 0;
+        // player - home
+        if(window.keyMap[38]) {
+            // console.log('UP');
+            window.game.stadium.playerHome.velocity.y = 0 - velocity;
+            startGame();
+        }
+        if(window.keyMap[40]) {
+            // console.log('DOWN');
+            window.game.stadium.playerHome.velocity.y = velocity;
+            startGame();
+        }
+        if(window.keyMap[37]) {
+            // console.log('LEFT');
+            window.game.stadium.playerHome.velocity.x = 0 - velocity;
+            startGame();
+        }
+        if(window.keyMap[39]) {
+            // console.log('LEFT');
+            window.game.stadium.playerHome.velocity.x = velocity;
+            startGame();
+        }
+        if(window.game.stadium.playerHome.velocity.x != 0 && window.game.stadium.playerHome.velocity.x != 0) {
+            window.game.stadium.playerHome.velocity.x /= Math.sqrt(2);
+            window.game.stadium.playerHome.velocity.y /= Math.sqrt(2);
+        }
     }
     if(window.keyMap[70]) {
         window.game.camera.showStats = !window.game.camera.showStats;
+    }
+    if(window.keyMap[80]) {
+        window.game.togglePause();
     }
 }
 
