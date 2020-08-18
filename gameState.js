71fd945ecad9f7dbf995575fda41c8e41cfe5072
Tibor Olah <42848540+olahtibi@users.noreply.github.com>
2020-08-18 16:01:21 -0400

    Delete gameState.js

--- gameState.js
+++
@@ -1,8 +0,0 @@
-var GameState = function (ballVelocity, playerHomeVelocity, playerAwayVelocity, lastUpdated, kickStarted) {
-    this.ballVelocity = ballVelocity;
-    this.playerHomeVelocity = playerHomeVelocity;
-    this.playerAwayVelocity = playerAwayVelocity;
-    this.currentTime = new Date().getTime();
-    this.lastUpdated = lastUpdated;
-    this.kickStarted = kickStarted;
-};
