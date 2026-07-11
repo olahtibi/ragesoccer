2dacd90d5e726588c0dfa3f4a74ea9d6927d8d8e
olahtibi <tibor.benjamin.olah@gmail.com>
2026-07-11 12:43:27 -0400

    revert css to test mobile flicker

--- tests/core/html.test.js
+++ tests/core/html.test.js
@@ -18,7 +18,7 @@ test("Options page contains strength and team-size controls", function() {
   assertTrue(html.indexOf('id="opponentStrength"') !== -1);
   assertTrue(html.indexOf('id="homeTeamSize"') !== -1);
   assertTrue(html.indexOf('id="awayTeamSize"') !== -1);
-  assertTrue(html.indexOf('href="styles/options.css') !== -1);
+  assertTrue(html.indexOf("<style>") !== -1);
   assertTrue(html.indexOf("1 - Red Novices") !== -1);
   assertTrue(html.indexOf("1 - Blue Novices") !== -1);
   assertTrue(html.indexOf("10 - Red Titans") !== -1);
@@ -29,12 +29,8 @@ test("Options page contains strength and team-size controls", function() {
 test("Game page contains canvas assets scripts and boot hook", function() {
   var html = readFile("game.html");
 
-  assertTrue(html.indexOf('onload="startGameWhenLandscape();"') !== -1);
-  assertTrue(html.indexOf('href="styles/game.css') !== -1);
+  assertTrue(html.indexOf('onload="startLoop();"') !== -1);
   assertTrue(html.indexOf('id="myCanvas"') !== -1);
-  assertTrue(html.indexOf('id="rotateNotice"') !== -1);
-  assertTrue(html.indexOf("function startGameWhenLandscape()") !== -1);
-  assertTrue(html.indexOf("startLoop();") !== -1);
   assertTrue(html.indexOf('src="assets/images/pitch.jpg"') !== -1);
   assertTrue(html.indexOf('src="src/core/configuration.js') !== -1);
   assertTrue(html.indexOf('src="src/ai/commands/commandRegistry.js') !== -1);
